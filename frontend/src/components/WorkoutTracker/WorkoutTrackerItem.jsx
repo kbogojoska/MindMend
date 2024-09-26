ee9ef3b97ec57d66a05f79247a483fb1be6f1b06
kbogojoska <107806557+kbogojoska@users.noreply.github.com>
2024-09-26 20:38:21 +0200

    refactor

--- frontend/src/components/WorkoutTracker/WorkoutTrackerItem.jsx
+++ frontend/src/components/WorkoutTracker/WorkoutTrackerItem.jsx
@@ -1,23 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import gifImage from "../../gifs/workout_tracker.gif";
 import "../../css/WorkoutTracker/WorkoutTracker.css";
 
 function WorkoutTrackerItem(props) {
-  // const [reminders, setReminders] = useState([]);
-
-  useEffect(() => {
-    const username = localStorage.getItem("loggedInUser");
-    if (username) {
-      // const userRemindersKey = `${username}_workout_reminders`;
-      // const storedReminders = JSON.parse(localStorage.getItem(userRemindersKey)) || [];
-      // setReminders(storedReminders);
-    }
-  }, []);
-
-  // const getReminderForWorkouts = (startWorkoutTime) => {
-  //   const reminder = reminders.find(r => r.message.includes(`Start at ${startWorkoutTime}`));
-  //   return reminder ? reminder.time : null;
-  // };
 
   return (
     <div className="item-container flex justify-center">
@@ -37,11 +22,6 @@ function WorkoutTrackerItem(props) {
             <li className="list-group-item">
               End of workout time: {props.endWorkoutTime}
             </li>
-            {/* <li className="list-group-item">
-              Reminder: {getReminderForWorkouts(props.startWorkoutTime) 
-                ? `${new Date(getReminderForWorkouts(props.startWorkoutTime)).toLocaleString()}` 
-                : 'No reminder set'}
-            </li> */}
           </ul>
         </div>
       </div>
